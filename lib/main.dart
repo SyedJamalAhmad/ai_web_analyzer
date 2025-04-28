b9b5e65fc1c66330f4b7947d118182eacfe5d7c9
Syed Jamal Ahmad <syedjamal969@gmail.com>
2025-04-28 15:29:58 +0500

    firebase analytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,25 @@
 import 'package:ai_web_analyzer/app/fire_base/fire_base.dart';
 import 'package:ai_web_analyzer/app/fire_base/remoteconfig_services.dart';
 import 'package:ai_web_analyzer/app/routes/app_pages.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 void main() async {
-  // WidgetsFlutterBinding.ensureInitialized();
-  // await MyFireBaseHandle.initialize();
-  // await RemoteConfigService().initialize();
+  WidgetsFlutterBinding.ensureInitialized();
+  await MyFireBaseHandle.initialize();
+  await RemoteConfigService().initialize();
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+      static FirebaseAnalytics analytics = FirebaseAnalytics.instance;
+  static FirebaseAnalyticsObserver observer =
+      FirebaseAnalyticsObserver(analytics: analytics);
+  
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
